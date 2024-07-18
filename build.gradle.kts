706de5430d373a7aad6ab13736e34f9184b9e0f0
Martin Bonnin <martin@mbonnin.net>
2024-07-18 13:49:43 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+import com.gradleup.librarian.gradle.librarianRoot
+
+plugins {
+  id("org.jetbrains.kotlin.jvm").version("2.0.0").apply(false)
+  id("com.gradleup.librarian").version("0.0.6-SNAPSHOT").apply(false)
+}
+
+librarianRoot()
